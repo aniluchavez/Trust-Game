62d731c213fb8576afa79968c843283571489a40
GeorgiosKokalas <kokalasg@gmail.com>
2024-10-29 10:26:20 -0500

    Made image path finding more less user-specific

--- scratchspace/Code/trust_main.py
+++ scratchspace/Code/trust_main.py
@@ -22,7 +22,12 @@
 
 # trust_main.py
 from experiment import run_experiment
+import os
+
 
 if __name__ == "__main__":
+    # Go to the directory of this script
+    os.chdir(f'{os.path.dirname(os.path.realpath(__file__))}/..')
+
     # Launch the experiment using the configuration specified in parameters.py
     run_experiment()
